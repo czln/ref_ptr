c7670753dbb8759c5f2b5c6a7300b41927dc46f2
czln <czlczln211@gmail.com>
2021-09-09 13:09:53 +0800

    bug fix on move operation, add reset(), better test case

--- ref_ptr.h
+++ ref_ptr.h
@@ -6,8 +6,6 @@
 
 /**
  *  @brief  a simple smart pointer that can handle array buffer
- *  @bug    valgrind report: error calling PR_SET_PTRACER, vgdb might block
- *          env: wsl
  */
 namespace ref {
 
@@ -26,10 +24,10 @@ namespace ref {
         T *p;
     public:
         _ref_cnt() : cnt(0), p(nullptr), deleter([](T *ptr){
-                                                delete ptr;}) { }
-        explicit _ref_cnt(T *ptr, 
-                            Deleter deleter=[](T *ptr){
-                                                delete ptr;}) {
+            delete ptr;}) { }
+        explicit _ref_cnt(T *ptr,
+                          Deleter deleter=[](T *ptr){
+                              delete ptr;}) {
             cnt = 1;
             this->deleter = deleter;
             p = ptr;
@@ -38,11 +36,11 @@ namespace ref {
 
         // not allowed to copy
         _ref_cnt(const _ref_cnt& lhs) = delete;
-        const _ref_cnt& operator=(const _ref_cnt& lhs) = delete;
+        _ref_cnt& operator=(const _ref_cnt& lhs) = delete;
 
         int add_ref();
         int dec_ref();
-        
+
         int get_cnt() { return cnt;}
         T&  get_ref(int i=0) {return *(p+i);}
         T*  get_ptr(int i=0) const { return p+i; }
@@ -61,7 +59,7 @@ namespace ref {
         // lock.unlock();
         return tmp;
     }
-    
+
 
 
     /**
@@ -89,8 +87,9 @@ namespace ref {
                 pcnt->add_ref();
             }
         }
-        const ref_ptr& operator=(const ref_ptr& lhs) {
+        ref_ptr& operator=(const ref_ptr& lhs) {
             if (this != &lhs) {
+                reset();
                 pcnt = lhs.pcnt;
                 pcnt->add_ref();
             }
@@ -101,44 +100,47 @@ namespace ref {
         ref_ptr(ref_ptr&& rhs) {
             if (this != &rhs) {
                 pcnt = rhs.pcnt;
-                rhs = nullptr;
+                pcnt->add_ref();
+                rhs.reset();
             }
         }
-        const ref_ptr& operator=(ref_ptr&& rhs) {
+        ref_ptr& operator=(ref_ptr&& rhs) {
             if (this != &rhs) {
+                reset();
                 pcnt = rhs.pcnt;
-                rhs = nullptr;
+                pcnt->add_ref();
+                rhs.reset();
             }
             return *this;
         }
         ~ref_ptr() {
             if (pcnt) {
-                if (!pcnt->dec_ref()) 
+                if (!pcnt->dec_ref())
                     delete pcnt;
             }
         }
 
-        operator bool() { return get(); }
+        explicit operator bool() { return get(); }
         T*  operator ->() {return pcnt->get_ptr(); }
         T*  get() { if(pcnt) return pcnt->get_ptr(); return nullptr;}
         int use_count() {return pcnt->get_cnt();}
         void reset() { // to empty
             if (pcnt) {
-                if (!pcnt->dec_ref()) 
+                if (!pcnt->dec_ref())
                     delete pcnt;
                 pcnt = nullptr;
             }
         }
         void reset(T* p) {
             if (pcnt) {
-                if (!pcnt->dec_ref()) 
+                if (!pcnt->dec_ref())
                     delete pcnt;
                 pcnt = p;
             }
         }
-        
+
     };
-    
+
     // friend functions
     template <typename T>
     T& operator *(const ref_ptr<T>& val) {
@@ -148,9 +150,13 @@ namespace ref {
 
     /** 
      *  @brief  specialized ref_ptr for array objects
-     *  TODO:   check if it works for class deleter, i.g.
-     *          if @p  std::function<void(T*)> deleter needs
-     *          to be a template typename
+     *  @NOTE:  there will be a memory leak if this is called like: 
+     *  bad \code
+     *  ref_ptr<char[]> ptr(new char);
+     *  \endcode
+     *  good \code
+     *  ref_ptr<char[]> ptr(new char[1]);
+     *  \endcode
      */
     template <typename T, typename Deleter>
     class ref_ptr<T[], Deleter> {
@@ -165,8 +171,9 @@ namespace ref {
          *  allow convertion from @c T* to @c ref_ptr<T*,Deleter>
          */
         ref_ptr() : pcnt(nullptr) { }
+        /// \fixme: this would cause mismatched free just like std::shared_ptr
         ref_ptr (T *p, std::function<void(T*)> deleter=[](T *p){delete []p;}) :
-            pcnt(new _ref_cnt<T, std::function<void(T*)>>(p, deleter)) {}
+                pcnt(new _ref_cnt<T, std::function<void(T*)>>(p, deleter)) { }
 
         // copy assignment
         ref_ptr(const ref_ptr& lhs) {
@@ -175,50 +182,54 @@ namespace ref {
                 pcnt->add_ref();
             }
         }
-        const ref_ptr& operator=(const ref_ptr& lhs) {
+        ref_ptr& operator=(const ref_ptr& lhs) {
             if (this != &lhs) {
+                reset();
                 pcnt = lhs.pcnt;
                 pcnt->add_ref();
             }
             return *this;
         }
-        
+
         // move assignment
         ref_ptr(ref_ptr&& rhs) {
             if (this != &rhs) {
                 pcnt = rhs.pcnt;
-                rhs.pcnt = nullptr;
+                pcnt->add_ref();
+                rhs.reset();
             }
         }
-        const ref_ptr& operator=(ref_ptr&& rhs) {
+        ref_ptr& operator=(ref_ptr&& rhs) {
             if (this != &rhs) {
+                reset();
                 pcnt = rhs.pcnt;
-                rhs.pcnt = nullptr;
+                pcnt->add_ref();
+                rhs.reset();
             }
             return *this;
         }
         // deconstructor
         ~ref_ptr() {
             if (pcnt) {
-                if (!pcnt->dec_ref()) 
+                if (!pcnt->dec_ref())
                     delete pcnt;
             }
         }
     public:
-        operator bool() { return get(); }
+        explicit operator bool() { return get(); }
         T&  operator [](int i) { return pcnt->get_ref(i); }
         T*  get(int i=0) { if (pcnt) return pcnt->get_ptr(i);  return nullptr; }
         int use_count() {return pcnt->get_cnt();}
         void reset() { // to empty
             if (pcnt) {
-                if (!pcnt->dec_ref()) 
+                if (!pcnt->dec_ref())
                     delete pcnt;
                 pcnt = nullptr;
             }
         }
         void reset(T* p) {
             if (pcnt) {
-                if (!pcnt->dec_ref()) 
+                if (!pcnt->dec_ref())
                     delete pcnt;
                 pcnt = p;
             }
@@ -235,7 +246,7 @@ namespace ref {
     /** TODO: like std::make faster than constructors*/
     template <typename T, typename D = std::function<void(T*)>>
     ref_ptr<T, D> make_ref(T *p, D d=[](T* _p){delete _p;}) {
-        return ref_ptr<T, D>(p);
+        return ref_ptr<T, D>(p, d);
     }
     /** TODO:*/
     // template <typename T, typename D = std::function<void(T*)>>
--- test.cpp
+++ test.cpp
@@ -1,5 +1,6 @@
 #include "ref_ptr.h"
-#include <iostream>
+#include <cstdio>
+// #include <initializer_list>
 using namespace ref;
 
 template <typename T>
@@ -8,7 +9,7 @@ private:
     T *a;
 public:
     test() : a(nullptr) { }
-    test(T b) { a = new int(b);}
+    test(T b) { a = new T(b);}
     T& get() {return *a;}
     ~test() {delete a;}
 
@@ -19,28 +20,49 @@ typedef struct {
     char b;
 } s;
 
+void basic_test() {
+    ref_ptr<int> i1(new int(1));
+    if (*i1 == 1)
+        printf("basic test <int> constructor pass\n");
+    ref_ptr<int> i2(i1);
+    if (*i2 == *i1)
+        printf("basic test <int> copy constructor pass\n");
+    ref_ptr<char> c2 = new char(2);
+    if (*c2 == 2)
+        printf("basic test <char> copy assignment pass\n");
+    ref_ptr<test<double>> td(new test<double>(1.1));
+    if (td->get() == 1.1)
+        printf("basic test <template class> constructor pass\n");
+    auto tdm = std::move(td);
+    if (tdm->get() == 1.1 && !td) {
+        printf("basic test <template class> move assignemnt pass\n");
+    }
+}
+
+void array_test() {
+    ref_ptr<int[]> i1(new int[2]{1,2});
+    if (i1[0] == 1 && i1[1] == 2)
+        printf("array test <int[]> constructor pass\n");
+    ref_ptr<int[]> i2(i1);
+    if (i2[0] == i1[0] && i2[1] == i1[1])
+        printf("array test <int[]> copy constructor pass\n");
+    ref_ptr<char[]> c2 = new char[1]{2};
+    if (c2[0] == 2)
+        printf("array test <char> copy assignment pass\n");
+    ref_ptr<test<double>[]> td(new test<double>[2]{1.1, 2.2});
+    if (td[0].get() == 1.1 && td[1].get() == 2.2)
+        printf("array test <template class> constructor pass\n");
+    auto tdm = std::move(td);
+    if (tdm[0].get() == 1.1 && tdm[1].get() == 2.2 && !td) {
+        printf("array test <template class> move assignemnt pass\n");
+    }
+
+}
+
 
 int main() {
-    ref_ptr<int[]> pa = new int [2];
-    ref_ptr<int[]> pb = std::move(ref_ptr<int[]>((new int [2])));;
-    ref_ptr<int[]> pc(pa);
-    pb[0] = pa[0] = 0;
-    pb[1] = pa[1] = 1;
-    std::cout << pb[1] <<std::endl;
+    basic_test();
+    array_test();
 
-    ref_ptr<int> a(new int (12));
-    ref_ptr<int> b;
-    if (a && !b) {
-        printf("bool test ok\n");
-    }
-    printf("%d\n", *a);
-    s sa;
-    sa.a = 1;
-    sa.b = 'a';
-    ref_ptr<s> ps(new s(sa));
-    printf("%d %c\n", ps->a, ps->b);
-
-    int *aa = a.get();
-    printf("%d\n", *aa);
     return 0;
 }
